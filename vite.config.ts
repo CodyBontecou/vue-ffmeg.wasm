3bf676800dc22fab4110cdd2369b41c6f2588f85
Cody Bontecou <bontecouc@gmail.com>
2024-03-16 22:47:06 -1000

    removed unused vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-// @ts-ignore
-import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), crossOriginIsolation()],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
