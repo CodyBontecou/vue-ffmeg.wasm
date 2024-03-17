207e45bffd8dc2886a467b93983d2f9adcf0df65
Cody Bontecou <bontecouc@gmail.com>
2024-03-16 22:25:36 -1000

    crossOrigin vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+// @ts-ignore
+import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), crossOriginIsolation()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
